d6c329e0f8b1013eac726fe184c7771503e4c32f
sonnguyen <anhson120402@gmail.com>
2023-08-20 11:51:35 +0700

    connect db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import databaseService from './services/database.service'
 
+dotenv.config()
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+databaseService.connect()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ src/services/database.service.ts
@@ -0,0 +1,33 @@
+import { MongoClient, Db, Collection } from 'mongodb'
+import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
+
+config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.koejvi9.mongodb.net/?retryWrites=true&w=majority`
+
+class DatabaseService {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+
+  async connect() {
+    try {
+      await this.db.command({ ping: 1 })
+
+      console.log('Connection successfully')
+    } finally {
+      await this.client.close()
+    }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection('users')
+  }
+}
+
+const databaseService = new DatabaseService()
+
+export default databaseService
